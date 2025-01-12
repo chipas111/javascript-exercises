1ef849aea84b669acacb10e458d32cf73dec1224
Dima <chipas111@gmail.com>
2025-01-12 17:04:34 +0800

    Finish excersice 01 Finish Excersise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+    return 'ERROR'
+    }
 
+    let greetings = ''
+    
+    for (let i = 0; i < num; i++)
+    greetings += string
+
+    return greetings
 };
+
+console.log(repeatString('hey', 3))
+console.log(repeatString('hello', 10))
+console.log(repeatString('hi', 1))
+console.log(repeatString('bye', 0))
+console.log(repeatString('goodbye', -1))
 
 // Do not edit below this line
 module.exports = repeatString;
